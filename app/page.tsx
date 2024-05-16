b1351c63f138e57ae0333c147ca719f170371dd5
Asharib Ali <asharibarain4@gmail.com>
2024-05-16 21:55:40 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { useEffect } from "react";
+
+const OwlAssistant = () => {
+  useEffect(() => {
+    // Check if the script is already loaded
+    if (
+      !document.querySelector(
+        'script[src="https://cdn.jsdelivr.net/gh/AsharibAli/FlowiseChatEmbed@latest/dist/web.js"]'
+      )
+    ) {
+      const script = document.createElement("script");
+      script.src =
+        "https://cdn.jsdelivr.net/gh/AsharibAli/FlowiseChatEmbed@latest/dist/web.js";
+      script.type = "module";
+      script.async = true;
+      script.onload = () => {
+        if (window.Chatbot) {
+          window.Chatbot.initFull({
+            chatflowid: "b4a8fdf2-8912-4d36-873d-b1ad8d6c97d1",
+            apiHost: "https://flowise-r1c3.onrender.com",
+            theme: {
+              button: {
+                backgroundColor: "#000000",
+                size: "medium",
+                iconColor: "white",
+                customIconSrc:
+                  "https://raw.githubusercontent.com/AsharibAli/project-images/main/owl_logo.png",
+              },
+              chatWindow: {
+                welcomeMessage:
+                  "Welcome to the Owl Assistant! I'm here to assist you with any questions you have about the Owl Protocol, Owl Assistant helps developers to build full-stack web3 applications with the help of Owl API.",
+                backgroundColor: "#ffffff",
+                fontSize: 16,
+                poweredByTextColor: "#000000",
+                botMessage: {
+                  backgroundColor: "#000000",
+                  textColor: "#ffffff",
+                  showAvatar: true,
+                  avatarSrc:
+                    "https://raw.githubusercontent.com/AsharibAli/project-images/main/owl_logo.png",
+                },
+                userMessage: {
+                  backgroundColor: "#000000",
+                  textColor: "#ffffff",
+                  showAvatar: true,
+                  avatarSrc:
+                    "https://raw.githubusercontent.com/AsharibAli/project-images/main/usericon.png",
+                },
+                textInput: {
+                  placeholder: "Type your question",
+                  backgroundColor: "#ffffff",
+                  textColor: "#000000",
+                  sendButtonColor: "#000000",
+                },
+              },
+            },
+          });
+        }
+      };
+      document.body.appendChild(script);
+    }
+  }, []);
+
+  return (
+    <div className="m-0">
+      <flowise-fullchatbot></flowise-fullchatbot>
+    </div>
+  );
+};
+
+export default OwlAssistant;
